54c280a65152392592e1eff132d194fbb0f5b925
TJS1 <avantikasarkar18@gmail.com>
2021-04-01 11:32:59 +0530

    tech-stack-section animation

--- web/main.js
+++ web/main.js
@@ -44,6 +44,55 @@ $("section").each(function(i) {
 });
 
 
+//section-3 GSAP animation
 
+var totalElements = 0;
+var elementAnimationDelay = 0;
+var elementCountUp = 0;
+
+function elementMoveUp(elementItem) {
+    console.log("move up called ")
+  TweenMax.to(elementItem, 1.3, {
+    top: "-=20px",
+    ease: Power2.easeIn,
+    onCompleteParams:[elementItem],
+    onComplete: elementMoveDown
+  });
+}
+
+function elementMoveDown(elementItem) {
+  console.log("element move down called")
+  if(elementCountUp != totalElements) {
+    elementCountUp++;
+  } else {
+    elementAnimationDelay = 0; //no delay after initial run
+  }
+
+  TweenMax.to(elementItem, 1.3, {
+    delay: elementAnimationDelay,
+    top: "+=20px",
+    ease: Power2.easeOut,
+    onCompleteParams:[elementItem],
+    onComplete: elementMoveUp
+  });
+}
+
+function initPhoneHovering() {
+    console.log("init Phone hover  called ")
+  $("#icon-container > div[id]").each(function(){
+    totalElements++;
+    elementAnimationDelay += 0.4;
+
+    var targetElement = $("#element-"+totalElements);
+
+    //init animation
+    elementMoveDown(targetElement);
+  });
+}
+
+$("button").on('click', function() {
+  console.log("button clicked")
+  initPhoneHovering();
+});
 
 
