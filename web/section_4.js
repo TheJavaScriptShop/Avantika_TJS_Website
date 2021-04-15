8c18f95b614c0e572be1ccd3c671e4c3fbd0be35
TJS1 <avantikasarkar18@gmail.com>
2021-04-15 18:21:51 +0530

    wip tjs_website/testimonial-section
    - fixed chat bubble orientation issue
    - added fixed-background/ parallax scroll for chat screen

--- web/section_4.js
+++ web/section_4.js
@@ -6,34 +6,78 @@ $(function () { // wait for document ready
     new ScrollMagic.Scene({
       triggerElement: "#section-4",
       triggerHook: 0.9, // show, when scrolled 10% into view
-      duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    //   duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
       offset: 50 // move trigger to center of element
     })
     .setClassToggle("#reveal1", "visible") // add class to reveal
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
-    
-    
+
     new ScrollMagic.Scene({
       triggerElement: "#reveal2",
       triggerHook: 0.9, // show, when scrolled 10% into view
-      duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    //   duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
       offset: 50 // move trigger to center of element
     })
     .setClassToggle("#reveal2", "visible") // add class to reveal
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
     
     
     new ScrollMagic.Scene({
       triggerElement: "#reveal3",
       triggerHook: 0.9, // show, when scrolled 10% into view
-      duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+    //   duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
       offset: 50 // move trigger to center of element
     })
     .setClassToggle("#reveal3", "visible") // add class to reveal
-    .addIndicators() // add indicators (requires plugin)
+    // .addIndicators() // add indicators (requires plugin)
     .addTo(controller);
+
+
+    new ScrollMagic.Scene({
+        triggerElement: "#reveal4",
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+      })
+      .setClassToggle("#reveal4", "visible") // add class to reveal
+      // .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+      console.log("animation 4 triggered")
+
+
+      new ScrollMagic.Scene({
+        triggerElement: "#reveal5",
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+      })
+      .setClassToggle("#reveal5", "visible") // add class to reveal
+      // .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+
+      new ScrollMagic.Scene({
+        triggerElement: "#reveal6",
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+      })
+      .setClassToggle("#reveal6", "visible") // add class to reveal
+      // .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
+      console.log("animation 4 triggered")
+      
+
+      new ScrollMagic.Scene({
+        triggerElement: "#reveal7",
+        triggerHook: 0.9, // show, when scrolled 10% into view
+        // duration: "80%", // hide 10% before exiting view (80% + 10% from bottom)
+        offset: 50 // move trigger to center of element
+      })
+      .setClassToggle("#reveal7", "visible") // add class to reveal
+      // .addIndicators() // add indicators (requires plugin)
+      .addTo(controller);
     
     
     });
